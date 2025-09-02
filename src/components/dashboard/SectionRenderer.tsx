3074c4f03347c4fda1dea828874b8eb08a7456e8
Ajay Kumar Chaturvedi <chaturvedikraj2109@gmail.com>
2025-09-02 06:27:21 +0530

    chore: Update ESLint rules and clean up imports in SectionRenderer component

--- src/components/dashboard/SectionRenderer.tsx
+++ src/components/dashboard/SectionRenderer.tsx
@@ -5,7 +5,6 @@ import React, { useEffect, useMemo, useState } from 'react'
 import GridLayout from 'react-grid-layout'
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
-
 import { Card } from '@/components/ui/card'
 
 import { widgetRegistry, type Widget } from './WidgetRegistry'
